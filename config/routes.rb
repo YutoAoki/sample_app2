bfd1d33973f2d0d349152189cb7243382b909e4d
YutoAoki <frcsnt@gmail.com>
2018-07-07 14:43:22 +0900

    Inpkement basic ligin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -6,7 +8,9 @@ Rails.application.routes.draw do
   get 'about', to:'static_pages#about'
   get 'signup', to:'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
   resources :users
 
 end
