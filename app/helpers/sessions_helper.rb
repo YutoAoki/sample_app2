b0a859507142edb547bb7256336ff8c235eec334
YutoAoki <frcsnt@gmail.com>
2018-07-08 16:53:20 +0900

    Rails tutorial 9 finished

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,12 +13,12 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
-    # else (user_id = cookies.signed[:user_id])
-    #   user = User.find_by(id: user_id)
-    #   if user && user.authenticated?(cookies[:remember_token])
-    #     log_in(user)
-    #     @current_user = user
-    #   end
+    else (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in(user)
+        @current_user = user
+      end
     end
   end
 
