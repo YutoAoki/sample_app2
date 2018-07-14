9359c55df8b3dde59e404533f4899a77684da51c
YutoAoki <frcsnt@gmail.com>
2018-07-14 13:51:48 +0900

    時間フォーマットの変更、Mile表示を１０件,dest追記

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module SampleApp2
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
-
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
