6d04d7f45ef8813ae30af3562eb9e99bdae0a0e3
YutoAoki <frcsnt@gmail.com>
2018-09-24 18:51:04 +0900

    隠しおみくじ追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/help', to:'static_pages#help'
   get 'about', to:'static_pages#about'
+  get 'omikuji', to: 'static_pages#omikuji'
   get 'signup', to:'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/login', to:'sessions#new'
