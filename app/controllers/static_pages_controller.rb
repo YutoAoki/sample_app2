7c9290e94d43f4464472091ef3982e2b8cce4b34
YutoAoki <frcsnt@gmail.com>
2018-10-02 21:50:59 +0900

    topページの改修作業

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    @user = current_user
+    if logged_in?
+      @topics = @user.topics
+
+      @non_approved = Relationship.where(["owner_id = ? and status = ?", current_user.id, 2])
+      @approved = Relationship.where(["user_id = ? and status = ?", @user.id, 1])
+    end
   end
 
   def help
